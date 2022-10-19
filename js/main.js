71d678f33acf2588aac2e4a26d3604ec596cc294
dav100ide <oreglio.dev@gmail.com>
2022-10-19 14:21:19 +0200

    fine (inizio bonus)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
+// chiedi all'user il nome
 const userName = prompt('qual è il tuo nome?');
-console.log(userName, typeof userName);
+// chiedi all'user il cognome
+const userSurname = prompt('qual è il tuo cognome?');
+// chiedi il colore preferito
+const userColor = prompt('e il tuo colore preferito?');
+// il messaggio finale
+document.querySelector('h2').innerHTML = `ecco la tua password: ${userName}${userSurname}${userColor} `;
